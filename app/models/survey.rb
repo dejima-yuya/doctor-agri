51b73776aa25115581c516ed5c07722b3127bc9f
dejima-yuya <jimajapan33@yahoo.co.jp>
2024-02-14 15:41:53 +0900

    surveyの作成時にバリデーションのエラーメッセージが重複して表示されていたため修正

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,8 +2,6 @@ class Survey < ApplicationRecord
   validates :title, presence: true, length: { maximum: 200 }
   validates :is_useful, inclusion: [true, false]
   validates :is_request, inclusion: [true, false]
-  validates :category_id, presence: true
-  validates :crop_id, presence: true
 
   belongs_to :user
   belongs_to :category
