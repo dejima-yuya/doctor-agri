7d76a224e6cefa78f2672a2828e06248ed5fbe04
Kouhei Tsuru <kktturu1993@gmail.com>
2024-02-16 15:59:03 +0900

    category,crop,trainingモデルスペック完了

---
+++ spec/models/crop_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Crop, type: :model do
+  describe "アソシエーションについて" do
+    it { should have_many(:surveys)}
+    it { should have_many(:trainings).dependent(:destroy)}
+  end
+  describe "バリデーションについて" do
+    it { should validate_presence_of(:title)}
+    it { should validate_length_of(:title).is_at_most(20) } 
+  end
+
+end
