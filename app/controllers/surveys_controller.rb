8fc8d1bfce135598d838bf977e420fe7b6352654
dejima-yuya <jimajapan33@yahoo.co.jp>
2024-02-18 08:08:52 +0900

    surveyのランキングを表示させるためのviewを新規に作成

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -74,6 +74,9 @@ class SurveysController < ApplicationController
     end
   end
 
+  def analysis
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_survey
