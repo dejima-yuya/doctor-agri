58811f85705dbb8e2ff540224f29296b373158e7
Kouhei Tsuru <kktturu1993@gmail.com>
2024-02-20 02:31:55 +0900

    Survey dynamic message implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //= require passwords
+//= require messages
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "./passwords"
+import "./messages"
 
 Rails.start()
 Turbolinks.start()
