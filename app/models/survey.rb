37c792c8fab3529467e1b779d41630288ca9e5da
dejima-yuya <jimajapan33@yahoo.co.jp>
2024-02-19 12:43:04 +0900

    surveyのバリデーションエラーメッセージが正しい日本語の文章になっていなかったので修正

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,11 +1,11 @@
 class Survey < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 200 }
-  validates :is_useful, inclusion: [true, false]
-  validates :is_request, inclusion: [true, false]
-
   belongs_to :user
   belongs_to :category
   belongs_to :crop
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :is_useful, inclusion: [true, false]
+  validates :is_request, inclusion: [true, false]
 
   def self.category_ranking
     Survey.joins(:category).select('categories.id as category_id, categories.title as category_title, count(surveys.id) as surveys_count').group('categories.id').order('surveys_count DESC')
