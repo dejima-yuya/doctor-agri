fb6fb0d5d95bd8a03f1dc15e3664af6fc6d7aea5
dejima-yuya <jimajapan33@yahoo.co.jp>
2024-02-26 12:44:52 +0900

    カテゴリーデータや作物データの削除時に関連したアンケートデータが削除されるようになっていなかったので修正

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,5 +1,5 @@
 class Crop < ApplicationRecord
-  has_many :surveys
+  has_many :surveys, dependent: :destroy
   has_many :trainings, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 20 }
